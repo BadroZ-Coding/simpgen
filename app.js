97a89c80638d876d94a61c67c6353d60d4cd8c03
BadroZ-Coding <xetake1@gmail.com>
2023-07-27 16:14:44 +0200

    Node.JS installer finished

--- app.js
+++ app.js
@@ -54,29 +54,44 @@ inquirer.prompt([
     console.log(`⏳ Generation of project ${project.type} ⏳`)
     if (project.type === "Node.JS") {
         if (project.template === "Nothing") {
-            shell.exec("git clone git@github.com:BadroZ-Coding/simpgen-nodejs-nothing.git")
-            console.log("✅ successfully cloned repository ✅")
-            shell.exec(`mv ./simpgen-nodejs-nothing ${project.name}`)
-            let packageJSON = {}
-            fs.readFile(`./${project.name}/package.json`, 'utf-8', (err, data) => {
-                if (err) {
-                    console.error('An error occurred while reading the file:', err)
-                } else {
-                    try {
-                        packageJSON = JSON.parse(data)
-                    } catch {
-                        console.error('Error while parsing the JSON file:', error);
-                    }
-                }
-            })
+            shell.exec(`mkdir ${project.name} && cd ./${project.name} && touch app.js && npm init --yes && mkdir src && touch src/config.json`)
+            console.log('✅ successfully generated and initialized ✅')
+        } else if (project.template === "Express API" && project.mongodb === "Yes") {
+            shell.exec(`git clone git@github.com:BadroZ-Coding/simpgen-nodejs-express-mongodb.git && mv ./simpgen-nodejs-express-mongodb ${project.name}`)
+            console.log('✅ successfully cloned repository ✅')
+
+            let configJSON = JSON.parse(fs.readFileSync(`./${project.name}/src/config.json`, 'utf-8'))
+            let packageJSON = JSON.parse(fs.readFileSync(`./${project.name}/package.json`, 'utf-8'))
+
+            configJSON.port = project.port
             packageJSON.name = project.name
-            fs.appendFile(`./${project.name}/package.json`, JSON.stringify(packageJSON), 'utf-8', (err) => {
-                if (err) {
-                    console.error('An error occurred while adding content to the file:', err);
-                } else {
-                    console.log("✅ successfully generated and initialized ✅")
-                }
-            })
+
+            fs.writeFileSync(`./${project.name}/src/config.json`, JSON.stringify(configJSON), 'utf-8')
+            fs.writeFileSync(`./${project.name}/package.json`, JSON.stringify(packageJSON), 'utf-8')
+
+            console.log('✅ successfully generated and initialized ✅')
+
+            shell.exec(`cd ${project.name} && npm i`)
+
+            console.log('✅ dependency successfully installed ✅')
+        } else {
+            shell.exec(`git clone git@github.com:BadroZ-Coding/simpgen-nodejs-express.git && mv ./simpgen-nodejs-express ${project.name}`)
+            console.log('✅ successfully cloned repository ✅')
+
+            let configJSON = JSON.parse(fs.readFileSync(`./${project.name}/src/config.json`, 'utf-8'))
+            let packageJSON = JSON.parse(fs.readFileSync(`./${project.name}/package.json`, 'utf-8'))
+
+            configJSON.port = project.port
+            packageJSON.name = project.name
+
+            fs.writeFileSync(`./${project.name}/src/config.json`, JSON.stringify(configJSON), 'utf-8')
+            fs.writeFileSync(`./${project.name}/package.json`, JSON.stringify(packageJSON), 'utf-8')
+
+            console.log('✅ successfully generated and initialized ✅')
+
+            shell.exec(`cd ${project.name} && npm i`)
+
+            console.log('✅ dependency successfully installed ✅')
         }
     }
 })
